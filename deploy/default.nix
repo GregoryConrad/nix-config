76b5891c69caab74ec102c63c6287b67fc66cd85
Gregory Conrad <gregorysconrad@gmail.com>
2026-01-10 13:47:44 -0500

    Add automatic deployment GitHub Action

--- deploy/default.nix
+++ deploy/default.nix
@@ -24,6 +24,7 @@ in
   user = "root";
   sshUser = "deploy"; # NOTE: this is the "deploy" user from /hosts/modules/management.nix
   nodes = {
+    # NOTE: also update /.github/workflows/deploy.yaml for new nodes
     optimus = mkNode "optimus" "x86_64-linux";
     rpi4 = mkNode "rpi4" "aarch64-linux";
   };
